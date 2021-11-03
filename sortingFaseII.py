0685aa171c219c2768a4b60a9fdc40e37ea0b9ba
RomiNahir <r_nahir@yahoo.com>
2021-11-03 15:35:24 -0300

    update quicksort

--- sortingFaseII.py
+++ sortingFaseII.py
@@ -12,7 +12,7 @@ def MergeSort(array):
     
         indexL = indexR = indexM = 0 # index for right, index for left, index for merge
         while(indexL < len(L) and indexR < len(R)):
-            if(L[indexL] < R[indexR]):
+            if(L[indexL] <= R[indexR]):
                 array[indexM] = L[indexL]
                 indexL += 1
             else:
@@ -32,27 +32,50 @@ def MergeSort(array):
             indexR += 1
             indexM += 1 
     
-def quickSort(array, indexPivot):
-    pivot = array[indexPivot]
-    for index in range(len(array)-1):
-        for loop in range(0,len(array)-1):
-            if array(loop) < pivot:
+def partition(array, low, indexPivot):
+    index = low-1
+    pivot = array[indexPivot]  #choose the pivot element
+    for loop in range(low,indexPivot):
+        if array[loop] <= pivot:  #element is less than pivot
                 index += 1 
-                array[loop] , array[index] = array[index] , array[loop] 
-        
-        array[index+1],pivot = pivot , array[index+1]    
+                (array[index] , array[loop]) = (array[loop] , array[index])
 
-# quickSort()
-# Aca segun el video hay que hacer lo mismo desde 
-# donde quedo el pivot, que sería index+1, hacia la derecha
-# y a la izquierda, me confunden los indices
-# no estoy segura si es quickSort desde 0 a index+1
-# y quicksort desde index+1 hasta el final.  
+    array[index+1],array[indexPivot] = array[indexPivot] , array[index+1]    
+    return(index+1)
+
+def QuickSort(array, low, indexPivot): 
+    # como tengo que partir derecha e izquierda del pivot
+    # index+1 divide el array
+    # ordenar desde el mas chico al pivot, y desde el pivot al mas grande
+
+    if low < indexPivot:
+        indexPartition = partition(array,low,indexPivot) # de aca obtengo el indice del pivot
+        
+        QuickSort(array,low,indexPartition-1)  # desde el inicio hasta el elemento que esta antes del pivot
+        QuickSort(array,indexPartition+1,indexPivot) #desde el elemento que esta despues del pivot y el pivot
 
 n= 10
 
-array = GenerateRandomArray(n)  
-print(array)
+arrayMerge = GenerateRandomArray(n)
 
-MergeSort(array)
-print(array)
+#print(arrayMerge)
+
+start = CurrentNanoSeconds()
+MergeSort(arrayMerge)
+end = CurrentNanoSeconds()
+executionTimeMerge = end-start
+
+arrayQuick = GenerateRandomArray(n)
+
+#print(arrayQuick)
+
+start = CurrentNanoSeconds()
+QuickSort(arrayQuick,0,len(arrayQuick)-1)
+end = CurrentNanoSeconds()
+executionTimeQuick = end-start
+
+print(f'Merge Sorted array: {arrayMerge}')                
+print(f'Merge sorting took: {executionTimeMerge} seconds')
+
+print(f'Quick Sorted array: {arrayQuick}')                
+print(f'Quick sorting took: {executionTimeQuick} seconds')
