0462d9b2952df9f88c7193a699785e749276eb90
RomiNahir <r_nahir@yahoo.com>
2021-11-22 17:06:19 -0300

    update binary search

--- searching.py
+++ searching.py
@@ -1,22 +1,52 @@
 from randomarray import GenerateRandomArray
 from nanoseconds import CurrentNanoSeconds
+from sorting import *
  
 
 def linearSearch(array,value):
     for i in range(len(array)):
+        print(array)
         if array[i] == value:
             indexSearch = i
+            return indexSearch
+
+def binarySearch(array,value):
+
+    high = len(array)
+    mid = high//2
+
+    if high > 0:
+        if array[mid] == value:
+            return 'found'
+        elif array[mid] < value:
+                low = mid + 1
+                array = array[low:high]
+                return binarySearch(array,value)
         else:
-            indexSearch = None
-    return indexSearch
+            array = array[0:mid]
+            return binarySearch(array,value)    
+    else:
+        return 'not found'
 
-n = 20
+lenArray = 20
+searchedElement = 41
 
-array = GenerateRandomArray(n)
+#array = GenerateRandomArray(lenArray)
+array = [41,25,84,62,2,7,12,4,37,62,41,33,72,96,145,-20,-7,0] #array for test
 
-linearIndex = linearSearch(array,5)
+sortedArray = InsertionSort(array) # sort array for binary search
 
-if linearIndex ==None:
-    print(f'Value is not in the array')
+linearIndex = linearSearch(array,searchedElement)
+
+print('Linear Search Algorithm')
+
+if linearIndex == None:
+    print(f'Value {searchedElement} is not in the array')
 else:
-    print(f'Value is at {linearIndex} index')        
+    print(f'Value {searchedElement} is at {linearIndex} index')        
+
+print('Binary Search Algorithm')
+
+binary = binarySearch(sortedArray,searchedElement)
+
+print(f'Value {searchedElement} is {binary}')
